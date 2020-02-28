539b572158ba52f022eab89971d9b65ca6efd400
NJalal7 <nahrin.jalal@vonage.com>
2020-02-27 19:15:54 -0500

    Fix for blog posts not rendering once deployed

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,6 +1,11 @@
 import { h } from 'preact';
+import { lazy, Suspense } from 'preact/compat';
 import style from './style';
-import Video from '../../components/video/video.js';
+
+let Video;
+if (typeof window !== 'undefined') {
+	Video = lazy(() => import('../../components/video/video.js'));
+}
 
 const photographs = (props) => {
 	return (
@@ -8,11 +13,11 @@ const photographs = (props) => {
 			<h1 class={style.pageTitle}>Hello.</h1>
 			<p>Enable your audio and video to begin.</p>
 			<div class={style.formWrapper}>
-				<Video />
+				<Suspense fallback={<div>loading...</div>}>
+					<Video />
+				</Suspense>
 			</div>
 		</div>
 	);
 };
-
 export default photographs;
-
